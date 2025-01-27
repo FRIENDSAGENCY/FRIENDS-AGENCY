6e2929605e2203974d6d54fdae53443e2c6459b9
FRIENDSAGENCY <suryasuryakumar293@gmail.com>
2025-01-28 02:27:55 +0530

    Delete FriendsJ.js

--- FriendsJ.js
+++
@@ -1,8 +0,0 @@
-// Example script to enhance interactivity
-document.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
-    });
-});
